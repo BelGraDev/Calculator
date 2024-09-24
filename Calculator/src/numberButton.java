5484952368c541cb3505d01de5e1730456865373
BelGraDev <carlosgranadosbeltran@gmail.com>
2024-09-24 18:34:13 +0200

    Classes done, let's create some buttons from calculator app

--- Calculator/src/numberButton.java
+++ Calculator/src/numberButton.java
@@ -1,18 +1,26 @@
 import javax.swing.*;
+import java.awt.event.*;
 
-public class numberButton{
+public class numberButton implements ActionListener{
 
-    private int WIDTH = 100;
+    private final int WIDTH= 100;
     private final int HEIGHT = 40;
 
     public int x = 0;
     public int y = 0;
     public int counter = 0;
+    private JButton jb;
+    private JTextField display;
 
-    public numberButton(String content){
+    public numberButton(String content, JFrame jf){
+
         int w = this.WIDTH;
-        JButton jb = new JButton(content);
+        jb = new JButton(content);
         jb.setBounds(x, y, w, this.HEIGHT);
+        jb.addActionListener(this);
+        display = new JTextField();
+        jf.add(jb);
+
 
         if(counter%3== 0){
             y+= 40;
@@ -20,5 +28,20 @@ public class numberButton{
         } 
         counter++;
         x+=w;
+
+
     }
+    public JButton getButton(){
+
+        return jb;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        String command = e.getActionCommand();
+        
+        // Append the clicked button text to the display
+        display.setText(display.getText() + command);
+    }
+
 }
--- Calculator/src/operationButton.java
+++ Calculator/src/operationButton.java
@@ -1,21 +1,37 @@
 import javax.swing.*;
+import java.awt.event.*;
 
-public class operationButton{
+public class operationButton implements ActionListener{
 
     private final int WIDTH = 50;
     private final int HEIGHT = 40;
 
 
+
     public int x = 300;
     public int y = 0;
-    public operationButton(String content){
+    private JButton jb;
+    private JTextField display;
 
-            JButton jb = new JButton(content);
-            jb.setBounds(x, y, this.WIDTH, this.HEIGHT);
+    public operationButton(String content, JFrame jf){
 
-            y+= this.HEIGHT;
+        jb = new JButton(content);
+        jb.setBounds(x, y, this.WIDTH, this.HEIGHT);
+        jb.addActionListener(this);
+        display = new JTextField();
+        jf.add(jb);
 
-        
+        y+= this.HEIGHT;
 
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        String command = e.getActionCommand();
+        
+        // Append the clicked button text to the display
+        display.setText(display.getText() + command);
+    }
+    
 }
